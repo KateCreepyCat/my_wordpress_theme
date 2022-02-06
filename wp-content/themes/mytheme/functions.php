a1c421f4a52c11c35acebf05646bfb5fdb909ee7
kate <kateryna.tomonova@gmail.com>
2022-02-06 16:19:24 +0200

    simple menu without styles

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,6 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'bootstrap_css' );
 add_action( 'wp_footer', 'bootstrap_js' );
+add_action('after_setup_theme', 'nav_menu');
 
 function bootstrap_css() {
 	wp_enqueue_style('style', get_stylesheet_uri());
@@ -11,4 +12,9 @@ function bootstrap_css() {
 function bootstrap_js() {
 	wp_register_script( 'bootstrap.min.js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' );
 	wp_enqueue_script( 'bootstrap.min.js' );
+}
+
+function nav_menu(){
+	register_nav_menu('header_menu', 'Header menu');
+	register_nav_menu('footer_menu', 'Footer menu');
 }
